408681d2b35a8f5cb7c652c1f6c7ea2118ec15ff
Administrator <Administrator@XROG98XNEE1N4Z3>
2019-06-28 10:36:11 +0800

    修

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -1,17 +1,12 @@
 package com.tedu.sp01.pojo;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
-	//1111111111111
-	//333333333333
-        //222222222222
-	//名称
+	//鍚嶇О
 	private Integer id;
 	private String name;
 	private Integer number;
